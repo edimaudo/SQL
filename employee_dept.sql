5fb92496cdc2859912889470146c413ab8a0ea9e
Edima Udo <edimaudo@gmail.com>
2018-05-08 10:06:57 -0400

    employee department script

--- employee_dept.sql
+++ employee_dept.sql
@@ -1,17 +1,22 @@
+#create employee table
 Create table EMPLOYEE (
-
 empid int,
 empname VARCHAR(50),
 mangerid int,
 deptid int,
 salary FLOAT,
 DOB date
-
-
 );
 
+#create department table
 Create table DEPARTMENT(
 deptid int,
 deptname varchar(10)
 );
 
+#load employee table
+
+#load department table
+
+
+#answer business questions
