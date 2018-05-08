7b6517793bc7ddc9603bedc198847d9f3bd6be07
Edima Udo <edimaudo@gmail.com>
2018-05-08 10:04:49 -0400

    employee and department sql script

---
+++ employee_dept.sql
@@ -0,0 +1,17 @@
+Create table EMPLOYEE (
+
+empid int,
+empname VARCHAR(50),
+mangerid int,
+deptid int,
+salary FLOAT,
+DOB date
+
+
+);
+
+Create table DEPARTMENT(
+deptid int,
+deptname varchar(10)
+);
+
