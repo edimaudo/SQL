feb68ecdf280509b391d008430733e5ffd5d4b66
Edima <edimaudo@gmail.com>
2020-12-11 23:35:20 -0500

    Update prodbasis.sql

--- prodbasis.sql
+++ prodbasis.sql
@@ -2,7 +2,8 @@
 set @Segment:="ACT";
 set @CurrentDate:=CURDATE();
 
-Select 
+Select *
 From prod p inner join basis b on p.`MODEL_TYPE` = b.`MODEL_TYPE`
 Where b.Segment = @Segment
-and p.`UNIT_STATUS` = 
+and p.`UNIT_STATUS` = "In Production"
+and p.`READINESS_DATE` <= @CurrentDate
