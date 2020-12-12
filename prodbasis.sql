22e429327bf08d8434881ad4bca17986c68c90f9
Edima <edimaudo@gmail.com>
2020-12-11 23:12:53 -0500

    Update prodbasis.sql

--- prodbasis.sql
+++ prodbasis.sql
@@ -1,1 +1,8 @@
-set @yourVariableName:=yourValue;
+#Set segment
+set @Segment:="ACT";
+set @CurrentDate:=CURDATE();
+
+Select 
+From prod p inner join basis b on p.`MODEL_TYPE` = b.`MODEL_TYPE`
+Where b.Segment = @Segment
+and p.`UNIT_STATUS` = 
